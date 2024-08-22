1948742657b7ce9d276ac7fba51209d0bf53e597
C Smith <cmis18@mun.ca>
2024-08-22 10:39:40 +0200

    1039changes

---
+++ Assets/Scripts/QuizManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class QuizManager : MonoBehaviour
+{
+    public List<QuestionsAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+    public GameObject Quizpanel;
+    public GameObject GOPanel;
+
+    public TextMeshProUGUI QuestionTxt;
+    public TextMeshProUGUI ScoreTxt;
+
+    int totalQuestions = 0;
+    public int score;
+
+    private void Start()
+    {
+        totalQuestions = QnA.Count;
+        GOPanel.SetActive(false);
+        generateQuestion();
+    }
+
+    public void retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void GameOver()
+    {
+        Quizpanel.SetActive(false);
+        GOPanel.SetActive(true);
+        ScoreTxt.text = score + "/" + totalQuestions;
+    }
+
+    public void correct()
+    {
+        //when you are right
+        score += 1;
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    public void wrong()
+    {
+        //when you answer wrong
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    void SetAnswers()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = QnA[currentQuestion].Answers[i];
+
+            if(QnA[currentQuestion].CorrectAnswer == i+1)
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            }
+        }
+    }
+
+    void generateQuestion()
+    {
+        if (QnA.Count > 0)
+        {
+            currentQuestion = Random.Range(0, QnA.Count);
+
+            QuestionTxt.text = QnA[currentQuestion].Question;
+            SetAnswers();
+        }
+        else 
+        {
+            Debug.Log("You Failed");
+            GameOver();
+        }
+            
+
+    }
+}
